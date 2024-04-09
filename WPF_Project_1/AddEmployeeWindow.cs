277d6345654fdca5c9e3dd6cd3d4fde5fcba058e
KeanuRo <91270196+KeanuRo@users.noreply.github.com>
2024-04-09 11:13:24 +0300

    init

---
+++ WPF_Project_1/AddEmployeeWindow.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace WPF_Project_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AddEmployeeWindow : Window
+    {
+        public AddEmployeeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPF_Project_1/EmployeeManagement.cs
@@ -0,0 +1,32 @@
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Project_1
+{
+
+    public partial class EmployeeManagement : Window
+    {
+        public EmployeeManagement()
+        {
+
+        }
+
+        private void DeleteEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            //  од дл€ удалени€ выбранного сотрудника
+        }
+
+        private void AddEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            //  од дл€ добавлени€ нового сотрудника
+        }
+    }
+}
